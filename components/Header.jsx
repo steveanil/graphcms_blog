293ce22897de8bae0790c6e3ef564f767cf34539
Steve <steveanil2003@gmail.com>
2022-11-25 22:04:37 -0800

    fixed bg color error

--- components/Header.jsx
+++ components/Header.jsx
@@ -50,7 +50,7 @@ const Header = () => {
         <div onClick={() => setToggle(!toggle)} className="text-3xl absolute right-2 top-7 cursor-pointer md:hidden">
           {toggle ? <BiX size={30} /> : <BiMenu size={30} /> }
         </div>
-        <ul className={`md:flex md:items-center md:pb-0 absolute md:static md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease in ${toggle ? 'top-20 opacity-100' : 'top-[-490px]'} md:opacity-100`}>
+        <ul className={`bg-white md:flex md:items-center md:pb-0 absolute md:static md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease in ${toggle ? 'top-20 opacity-100' : 'top-[-490px]'} md:opacity-100`}>
           {categories.map((category, index) => (
             <Link key={index} href={`/category/${category.slug}`} className="flex col-span-1 md:ml-8 text-lg md:my-0 my-7 cursor-pointer text-gray-800 hover:text-blue-700 duration-500 ">
               {category.name}
